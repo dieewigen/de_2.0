32bcdaefaa5d3e8bdbda6d97ed224f42ca4af71a
git <issomad@die-ewigen.com>
2021-06-04 14:02:45 +0200

    Anrede/Rechtschreibfehler gefixt

--- inc/lang/1_ally.registerzwei.lang.php
+++ inc/lang/1_ally.registerzwei.lang.php
@@ -1,15 +1,15 @@
 <?php
 $allyregisterzwei_lang['title']='Allinz registrieren';
 
-$allyregisterzwei_lang['msg_1']='Bitte kontrollieren sie ihren Allianznamen';
-$allyregisterzwei_lang['msg_2']='Bitte Kontrollieren sie ihre Allianzk&uuml;rzel';
+$allyregisterzwei_lang['msg_1']='Bitte kontrolliere deinen Allianznamen';
+$allyregisterzwei_lang['msg_2']='Bitte Kontrolliere dein Allianzk&uuml;rzel';
 $allyregisterzwei_lang['msg_3']='Das Allianzk&uuml;rzel ist zu lang';
-$allyregisterzwei_lang['msg_4']='Leider existiert schon eine Allianz mit ihrem K&uuml;rzel';
-$allyregisterzwei_lang['msg_5']='Leider existiert schon eine Allianz mit ihrem Namen';
-$allyregisterzwei_lang['msg_6']='Zuerst musst Du leider das Amt als Anf&uuml;hrer deiner alten Allianz an jemand anders &uuml;bergeben [Allianzmen&uuml]';
-$allyregisterzwei_lang['msg_7']='Eintragung erfolgreich. Ihre Allianz wurde nun hinzugef&uuml;gt.';
+$allyregisterzwei_lang['msg_4']='Leider existiert schon eine Allianz mit diesem K&uuml;rzel';
+$allyregisterzwei_lang['msg_5']='Leider existiert schon eine Allianz mit diesem Namen';
+$allyregisterzwei_lang['msg_6']='Zuerst musst du leider das Amt als Anf&uuml;hrer deiner alten Allianz an jemand anders &uuml;bergeben [Allianzmen&uuml]';
+$allyregisterzwei_lang['msg_7']='Eintragung erfolgreich. Deine Allianz wurde nun hinzugef&uuml;gt.';
 $allyregisterzwei_lang['msg_8_1']='Die Allianz';
 $allyregisterzwei_lang['msg_8_2']='wurde von';
 $allyregisterzwei_lang['msg_8_3']='gegr&uuml;ndet';
-$allyregisterzwei_lang['msg_9']='Wir k&ouml;nnen ihre Allianz leider nicht eintragen.<br><b>Begr&uuml;ndung:</b><br>';
+$allyregisterzwei_lang['msg_9']='Wir k&ouml;nnen deine Allianz leider nicht eintragen.<br><b>Begr&uuml;ndung:</b><br>';
 ?>
