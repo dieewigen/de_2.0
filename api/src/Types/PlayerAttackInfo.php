1d29d105cb2bbd8c9a133b3bfac6b3d51d8ea989
cJlD2ENp4PoPQ <cjld2enp4popq@googlemail.com>
2025-09-08 20:27:44 +0200

    fix:(API PlayerAttackInfo) fix syntax error

--- api/src/Types/PlayerAttackInfo.php
+++ api/src/Types/PlayerAttackInfo.php
@@ -8,20 +8,18 @@ class PlayerAttackInfo implements JsonSerializable {
     private int $id;
     private int $sector;
     private int $system;
-    private string $name;
     private int $points;
     private int $fleetPoints;
     private int $collectors;
     private int $race;
     private bool $canBeAttacked;
 
-    public function __construct(int $id, int $sector, int $system, string $name, int $points, int $fleetPoints,
+    public function __construct(int $id, int $sector, int $system, int $points, int $fleetPoints,
                                 int $collectors, int $race, bool $canBeAttacked)
     {
         $this->id = $id;
         $this->sector = $sector;
         $this->system = $system;
-        $this->name = $name;
         $this->points = $points;
         $this->fleetPoints = $fleetPoints;
         $this->collectors = $collectors;
@@ -31,7 +29,7 @@ class PlayerAttackInfo implements JsonSerializable {
 
     public function jsonSerialize(): array
     {
-        return [ 'id' => $this->id, 'sector' => $this->sector, 'system' => $this->system, 'name' => $this->name,
+        return [ 'id' => $this->id, 'sector' => $this->sector, 'system' => $this->system,
             'points' => $this->points, 'fpoints' => $this->fleetPoints, 'cols' => $this->collectors,
             'canBeAttacked' => $this->canBeAttacked, 'race' => $this->race];
     }
