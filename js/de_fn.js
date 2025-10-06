e35f630cf9730b65c5f97dfa0756be92defdbc3f
git <issomad@die-ewigen.com>
2025-10-06 11:49:27 +0200

    feat(Tooltip): verbessere Tooltip-Logik und füge Unterstützung für Touch-Geräte hinzu

--- js/de_fn.js
+++ js/de_fn.js
@@ -3,83 +3,119 @@ $( document ).ready(function() {
 });
 
 function setTooltip(){
-    //var targets = $( '[rel~=tooltip]' ),
     var targets = $('[title]'),
         target  = false,
         tooltip = false,
         title   = false;
  
-    targets.unbind();
-    targets.bind( 'mouseenter', function()
+    targets.unbind('mouseenter mouseleave touchstart');
+    targets.bind('mouseenter', function()
     {
-        target  = $( this );
-        tip     = target.attr( 'title' );
+        target  = $(this);
+        tip     = target.attr('title');
         if (tip) {
             tip = tip.replace('&', '<br>');
         }
-        tooltip = $( '<div id="tt"></div>' );
+        
+        // Entferne vorhandene Tooltips
+        $('#tt').remove();
+        
+        tooltip = $('<div id="tt"></div>');
  
-        if( !tip || tip == '' )
+        if(!tip || tip == '')
             return false;
  
-        target.removeAttr( 'title' );
-        tooltip.css( 'opacity', 0 )
-               .html( tip )
-               .appendTo( 'body' );
+        target.removeAttr('title');
+        target.data('original-title', tip); // Titel sicher speichern
+        
+        tooltip.css('opacity', 0)
+               .html(tip)
+               .appendTo('body');
  
         var init_tooltip = function()
         {
-            if( $( window ).width() < tooltip.outerWidth() * 1.5 )
-                tooltip.css( 'max-width', $( window ).width() / 2 );
+            if($(window).width() < tooltip.outerWidth() * 1.5)
+                tooltip.css('max-width', $(window).width() / 2);
             else
-                tooltip.css( 'max-width', 340 );
+                tooltip.css('max-width', 340);
  
-            var pos_left = target.offset().left + ( target.outerWidth() / 2 ) - ( tooltip.outerWidth() / 2 ),
+            var pos_left = target.offset().left + (target.outerWidth() / 2) - (tooltip.outerWidth() / 2),
                 pos_top  = target.offset().top - tooltip.outerHeight() - 20;
  
-            if( pos_left < 0 )
+            if(pos_left < 0)
             {
                 pos_left = target.offset().left + target.outerWidth() / 2 - 20;
-                tooltip.addClass( 'left' );
+                tooltip.addClass('left');
             }
             else
-                tooltip.removeClass( 'left' );
+                tooltip.removeClass('left');
  
-            if( pos_left + tooltip.outerWidth() > $( window ).width() )
+            if(pos_left + tooltip.outerWidth() > $(window).width())
             {
                 pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
-                tooltip.addClass( 'right' );
+                tooltip.addClass('right');
             }
             else
-                tooltip.removeClass( 'right' );
+                tooltip.removeClass('right');
  
-            if( pos_top < 0 )
+            if(pos_top < 0)
             {
-                var pos_top  = target.offset().top + target.outerHeight();
-                tooltip.addClass( 'top' );
+                pos_top = target.offset().top + target.outerHeight();
+                tooltip.addClass('top');
             }
             else
-                tooltip.removeClass( 'top' );
+                tooltip.removeClass('top');
  
-            tooltip.css( { left: pos_left, top: pos_top } )
-                   .animate( { top: '+=10', opacity: 1 }, 50 );
+            tooltip.css({left: pos_left, top: pos_top})
+                   .animate({top: '+=10', opacity: 1}, 50);
         };
  
         init_tooltip();
-        $( window ).resize( init_tooltip );
+        $(window).resize(init_tooltip);
  
         var remove_tooltip = function()
         {
-            tooltip.animate( { top: '-=10', opacity: 0 }, 50, function()
+            // Window resize unbinden
+            $(window).unbind('resize', init_tooltip);
+            
+            tooltip.animate({top: '-=10', opacity: 0}, 50, function()
             {
-                $( this ).remove();
+                $(this).remove();
             });
  
-            target.attr( 'title', tip );
+            // Titel aus data-Attribut wiederherstellen
+            var originalTitle = target.data('original-title');
+            if(originalTitle) {
+                target.attr('title', originalTitle);
+            }
+            
+            // Event-Listener entfernen
+            target.unbind('mouseleave', remove_tooltip);
+            tooltip.unbind('click touchstart', remove_tooltip);
         };
  
-        target.bind( 'mouseleave', remove_tooltip );
-        tooltip.bind( 'click', remove_tooltip );
+        target.bind('mouseleave', remove_tooltip);
+        tooltip.bind('click touchstart', remove_tooltip);
+    });
+    
+    // Zusätzlicher Fallback: Entferne alle Tooltips bei Scroll oder Resize
+    $(window).on('scroll resize', function() {
+        $('#tt').remove();
+        $('[data-original-title]').each(function() {
+            var $this = $(this);
+            $this.attr('title', $this.data('original-title'));
+        });
+    });
+    
+    // Touch-Geräte: Tooltip bei Touch außerhalb entfernen
+    $(document).on('touchstart', function(e) {
+        if(!$(e.target).closest('#tt').length && !$(e.target).closest('[title], [data-original-title]').length) {
+            $('#tt').remove();
+            $('[data-original-title]').each(function() {
+                var $this = $(this);
+                $this.attr('title', $this.data('original-title'));
+            });
+        }
     });
 };
 
