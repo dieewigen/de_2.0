67c5840291e9fbac917a0ec2871e8a3058b740a0
cJlD2ENp4PoPQ <cjld2enp4popq@popq.de>
2026-05-25 17:49:07 +0200

    add ally_war.php, ally_antrag to API whitelist

--- api/src/Model/ValidateGameFilename.php
+++ api/src/Model/ValidateGameFilename.php
@@ -17,7 +17,9 @@ class ValidateGameFilename
 		'secstatus.php',
 		'ally_register2.php',
 		'ally_join.php',
-		'sysnews.php'
+		'sysnews.php',
+        'ally_war.php',
+        'ally_antrag.php'
 	);
 
     public function isValid($filename) :bool {
