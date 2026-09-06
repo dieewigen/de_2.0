b5ea1dc02b69ac2cadd1673e88a0ce70aa82bf27
cJlD2ENp4PoPQ <cjld2enp4popq@popq.de>
2026-09-06 15:39:35 +0200

    feat(API Sector Status): add fleet target if target is shared

--- api/src/Types/SectorFleet.php
+++ api/src/Types/SectorFleet.php
@@ -5,16 +5,20 @@ namespace DieEwigen\Api\Types;
 use JsonSerializable;
 
 class SectorFleet implements JsonSerializable {
+    private int $targetSector;
+    private int $targetSystem;
     private int $sourcePlayerSector;
     private int $sourcePlayerSystem;
     private int $eta;
     private int $fp;
     private int $action;
 
-    public function __construct(int $sourcePlayerSector, int $sourcePlayerSystem, int $eta, int $fp, int $action)
+    public function __construct(int $targetSector, int $targetSystem, int $sourcePlayerSector, int $sourcePlayerSystem, int $eta, int $fp, int $action)
     {
         $this->sourcePlayerSector = $sourcePlayerSector;
         $this->sourcePlayerSystem = $sourcePlayerSystem;
+        $this->targetSector = $targetSector;
+        $this->targetSystem = $targetSystem;
         $this->eta = $eta;
         $this->fp = $fp;
         $this->action = $action;
@@ -22,7 +26,8 @@ class SectorFleet implements JsonSerializable {
 
     public function jsonSerialize(): array
     {
-        return ['sPlayerSec' => $this->sourcePlayerSector, 'sPlayerSys' => $this->sourcePlayerSystem, 'eta' => $this->eta, 'fp' => $this->fp,
-            'action' => $this->action];
+        return ['sTargetSec' => $this->targetSector, 'sTargetSys' => $this->targetSystem,
+            'sPlayerSec' => $this->sourcePlayerSector, 'sPlayerSys' => $this->sourcePlayerSystem, 'eta' => $this->eta,
+            'fp' => $this->fp, 'action' => $this->action];
     }
 }
